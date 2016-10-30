216d4612692896467354528915c47d18fa3e6458
tertychnyy <itertychnyy@gmail.com>
2016-10-30 21:10:04 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'chatfirst',
+  packages = ['chatfirst'],
+  version = '0.1',
+  description = 'Chatfirst client lib',
+  author = 'Ivan Tertychnyy',
+  author_email = 'it@chatfirst.co',
+  url = 'https://github.com/tertychnyy/chatfirst',
+  download_url = 'https://github.com/tertychnyy/chatfirst/tarball/v0.1',
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
