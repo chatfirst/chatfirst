46b8a217ab7c9db7263fc361938b7695d7083dd8
tertychnyy <itertychnyy@gmail.com>
2016-10-31 12:27:28 +0300

    packaged

--- setup.py
+++ setup.py
@@ -3,12 +3,10 @@ setup(
   name='chatfirst',
   version='0.1.0',
   description='Chatfirst Python Client',
-  long_description=open('README.md').read(),
   author='Ivan Tertychnyy',
   author_email='it@chatfirst.co',
   packages=['chatfirst'],
   url='https://github.com/chatfirst/chatfirst',
-  download_url='https://github.com/chatfirst/chatfirst/tarball/v0.1.0',
   license='LICENSE.txt',
   install_requires=[
         "requests>=2.10.0",
