c89325c1331f527e16f1329415c75ad9e4eb615d
tertychnyy <itertychnyy@gmail.com>
2016-10-31 12:09:07 +0300

    packaged

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# c1cli
+=======================
+Chatfirst Python Client
+=======================
+
+Chatfirst Python Client provides simple interface to Chatfirst API (https://api.chatfirst.co). Typical usage
+often looks like this::
+
+    #!/usr/bin/env python
+
+    from chatfirst.client import Chatfirst
+
+    client = Chatfirst(your_chatfirst_user_token)
+    my_bots = client.bots_list()
+
+See more in documentation
--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
-from distutils.core import setup
+from setuptools import setup
 setup(
-  name = 'chatfirst',
-  packages = ['chatfirst'],
-  version = '0.1',
-  description = 'Chatfirst client lib',
-  author = 'Ivan Tertychnyy',
-  author_email = 'it@chatfirst.co',
-  url = 'https://github.com/tertychnyy/chatfirst',
-  download_url = 'https://github.com/tertychnyy/chatfirst/tarball/v0.1',
-  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
-  classifiers = [],
+  name='chatfirst',
+  version='0.1.0',
+  description='Chatfirst Python Client',
+  long_description=open('README.md').read(),
+  author='Ivan Tertychnyy',
+  author_email='it@chatfirst.co',
+  packages=['chatfirst'],
+  url='https://github.com/chatfirst/chatfirst',
+  download_url='https://github.com/chatfirst/chatfirst/tarball/v0.1.0',
+  license='LICENSE.txt',
+  install_requires=[
+        "requests==2.10.0",
+    ],
 )
