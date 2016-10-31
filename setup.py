9344b90bff279c17f721e7cd51ba1cbd299cfda7
tertychnyy <itertychnyy@gmail.com>
2016-10-31 15:56:34 +0300

    packaged

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
   name='chatfirst',
-  version='0.1.0',
+  version='0.1.0a',
   description='Chatfirst Python Client',
   author='Ivan Tertychnyy',
   author_email='it@chatfirst.co',
